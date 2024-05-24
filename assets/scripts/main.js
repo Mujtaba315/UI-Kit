05c6aef9ac68e29706a737ffbacb7f7332b6c42e
Syed Muhammad Mujtaba Naqvi <123009259+Mujtaba315@users.noreply.github.com>
2024-05-24 06:59:45 -0700

    Add files via upload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -76,3 +76,60 @@ $('.input-div.google-search.123 ul li.123').click(function(){
     $("input.google-search-input.blue-border.123").attr("value", $(this).html());
 });
 
+// Day 24 / 5 / 24
+
+// Tabs
+
+// Tab 1 top
+
+//hiding all the tab buttons texts
+$('.tabs-text').css('display', 'none');
+
+// Displaying the text of active tab button
+$('#' + $('.tabs-btn.active').attr('id') + '-text').css('display', 'block');
+
+// Adding click funtion to all tab buttons
+$('.tabs-btn').click(function(){
+
+    // Buttons other than dropdown
+    if (!($(this).attr('id') == "tab1-dropdown")){  
+
+        // Remove active effect from all tab buttons
+        $('.tabs-btn').removeClass('active');  
+
+        // Adding active effect to the clicked button
+        $(this).addClass('active');
+
+        // Again hiding all tab buttons text because active button text is visible
+        $('.tabs-text').css('display', 'none');
+
+        //Displaying the text of clicked button
+        $('#' + $(this).attr('id') + '-text').css('display', 'block');
+
+        // Closing the dropdown when clicked on tab buttons other than dropdown button
+        $('.tabs .tab-dropdown').removeClass('open');
+    
+    // Dropdown button
+    } else {
+
+        // Removing active effect from all buttons
+        $('.tabs-btn').removeClass('active');
+
+        // Adding active effect to the clicked button (i.e dropdown button)
+        $(this).addClass('active');
+
+        // Open / close dropdown
+        $('.tabs .tab-dropdown').toggleClass('open');
+    }
+});
+
+
+// Adding click function to all dropdown items
+$('.tab-dropdown-item').click(function(){
+
+    // Hiding all the tabs buttons text
+    $('.tabs-text').css('display', 'none');
+
+    // Displaying the text of clicked list item
+    $('#' + $(this).attr('id') + '-text').css('display', 'block');
+});
